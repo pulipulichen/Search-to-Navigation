b4fed14d83548b399224d79960214e2c422af9e8
Pulipuli Chen <pulipuli.chen@gmail.com>
2018-03-12 13:16:27 +0800

    0312 好，加上去了

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Search-to-Navigation
+
+https://pulipulichen.github.io/Search-to-Navigation/
+
+https://pulipulichen.github.io/Search-to-Navigation/?search=keyword
---
+++ script.js
@@ -0,0 +1,38 @@
+var getQueryVariable = function(variable, url) {
+  if (url === undefined) {
+      url = window.location.search;
+  }
+  var query = url.substring(1);
+  var vars = query.split("&");
+  for (var i=0;i<vars.length;i++) {
+    var pair = vars[i].split("=");
+    if (pair[0] === variable) {
+      return pair[1];
+    }
+  } 
+  //alert('Query Variable ' + variable + ' not found');
+  return undefined;
+}
+
+// -----------------
+
+var _search = getQueryVariable("search");
+
+//alert(_search);
+
+if (_search.indexOf("pgfu") > -1 
+            && _search.indexOf("n.tw") > -1) {
+        //alert(decodeURIComponent(_search));
+        _search = "?" + decodeURIComponent(_search).split("?")[1];
+        
+        var _lat = getQueryVariable("lat", _search);
+        var _lon = getQueryVariable("lon", _search);
+        _search = _lat + "," + _lon;
+}
+
+// https://www.google.com/maps/dir/?api=1&destination=25.0795219204473,121.615607575112
+if (_search !== undefined && _search.trim() !== "") {
+    var _navigation_url = "https://www.google.com/maps/dir/?api=1&destination=" + _search;
+    //alert(_navigation_url);
+    window.open(_navigation_url);
+}
